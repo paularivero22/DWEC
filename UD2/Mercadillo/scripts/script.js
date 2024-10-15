bc1e345065f6ab5388b53ed27df1c8185e7a1550
unknown <daw2@IESMHP.LOCAL>
2024-10-15 16:26:00 +0200

    mercadillo

--- UD2/Mercadillo/scripts/script.js
+++ UD2/Mercadillo/scripts/script.js
@@ -1,143 +1,201 @@
 'use strict';
 
-let productos = {};
+const $negocio = (function () {
 
-/*
-Función “agregarProducto(nombre, cantidad, precio, categoría)” que añada el 
-producto si no existe. Si el producto ya existe muestra un alert indicando que
-el producto ya existe.
-*/
-function agregarProducto(nombre, cantidad, precio, categoria) {
+    let productos = {};
 
-    if (nombre in productos) {
-        alert("El producto ya existe");
-        return;
+    /*
+    Función “agregarProducto(nombre, cantidad, precio, categoría)” que añada el 
+    producto si no existe. Si el producto ya existe muestra un alert indicando que
+    el producto ya existe.
+    */
+    function agregarProducto(nombre, cantidad, precio, categoria) {
+
+        if (nombre in productos) {
+            alert("El producto ya existe");
+            return;
+        }
+
+        let producto = {
+            cantidad: cantidad,
+            precio: precio,
+            categoria: categoria,
+        };
+
+        productos[nombre] = producto;
+        alert('Producto ' + nombre + ' añadido');
     }
 
-    let producto = {
-        cantidad: cantidad,
-        precio: precio,
-        categoria: categoria,
+    /*
+    Función “eliminarProducto(nombre)” que elimina el producto del inventario. 
+    Si el producto no existe muestra un alert indicando que producto no existe.
+    */
+    function eliminarProducto(nombre) {
+        if (nombre in productos) {
+            delete (productos[nombre]);
+            alert(nombre + ' eliminado');
+        } else {
+            alert('No existe el producto');
+        }
+    }
+    /*
+    Función “buscarProducto(nombre)” que devuelve 
+    el objeto con la información del producto.
+    */
+    function buscarProducto(nombre) {
+        if (nombre in productos) {
+            alert(
+                'Datos del producto ' + "\n" + "\n"
+                + 'Nombre: ' + nombre + "\n"
+                + 'Cantidad: ' + productos[nombre].cantidad + "\n"
+                + 'Precio: ' + productos[nombre].precio + "\n"
+                + 'Categoria: ' + productos[nombre].categoria + "\n"
+            );
+        } else {
+            alert('No existe ese producto');
+        }
+    }
+
+    /* 
+    Función “actualizarInventario(nombre, cantidad)” incrementa o decrementa 
+    el stock según la cantidad indicada. Si la reducción deja el stock a 0 se 
+    mostrará un alert solicitando la reposición. 
+    */
+    function actualizarInventario(nombre, cantidad) {
+        if (nombre in productos) {
+            productos[nombre].cantidad += cantidad;
+            alert('Producto actualizado ');
+            buscarProducto(nombre);
+
+            if (productos[nombre].cantidad == 0) {
+                alert('No hay stock, se necesita reposicion');
+            }
+        } else {
+            alert('No existe ese producto, no se realizara ningun cambio');
+        }
+    }
+
+    /*
+    Función “ordenarProductosPorPrecio()” devuelve una lista ordenada de forma
+    ascendente. El resultado es un nuevo array, cuidado que no tienes el nombre.
+    */
+    function ordenarProductosPorPrecio() {
+        let datosEnArray = Object.entries(productos);
+        let datosFormateados = datosEnArray.map(([nombre, valor]) => ({ nombre, ...valor }));
+        let datosOrdenados = datosFormateados.sort((a, b) => a.precio - b.precio);
+        alert('Datos ordenados por precio');
+        alert(imprimirInventarioOrdenado(datosOrdenados));
+    }
+
+    /*function ordenarProductosPorPrecio() {
+        return Object.entries(productos)
+            .map(([nombre, valor]) => ({ nombre, ...valor }))
+            .sort((a, b) => a.precio - b.precio);
+    }*/
+
+    
+
+    /*
+    Función “imprimirInventario()” devuelve una lista con el informe de inventario, 
+    el cual muestra el “nombre”, “categoría”, “cantidad”, “precio” y “total” 
+    (cantidad*precio).
+    */
+    function imprimirInventario() {
+        for (let nombre in productos) {
+            let producto = productos[nombre];
+            alert(
+                "Datos de " + nombre + ": " + "\n" +
+                "Nombre: " + nombre + "\n" +
+                "Cantidad: " + producto.cantidad + "\n" +
+                "Precio: " + producto.precio + "\n" +
+                "Categoría: " + producto.categoria + "\n" +
+                "Total: " + (producto.cantidad * producto.precio) + "\n"
+            );
+        }
+    }
+
+    //hago un imprimir inventario para el array ordenado
+    function imprimirInventarioOrdenado(array2) {
+        let reporte = '';
+        Object.entries(array2).forEach(([nombre, element]) => {
+            reporte += 'Producto: ' + element.nombre +
+                '\nCategoría: ' + element.categoria +
+                '\nCantidad: ' + element.cantidad +
+                '\nPrecio: ' + element.precio +
+                '\nTotal: ' + (element.precio * element.cantidad) +
+                '\n\n';
+        });
+        alert(reporte); // Devuelve el informe
     };
 
-    productos[nombre] = producto;
-    alert('Producto ' + nombre + ' añadido');
-}
+    function filtrarProductosPorCategoria(categoria) {
+        let productosCategoria = "Productos de la categoria: " + categoria + "\n" + "\n";
 
-/*
-Función “eliminarProducto(nombre)” que elimina el producto del inventario. 
-Si el producto no existe muestra un alert indicando que producto no existe.
-*/
-function eliminarProducto(nombre) {
-    if (nombre in productos) {
-        delete (productos[nombre]);
-        alert(nombre + ' eliminado');
-    } else {
-        alert('No existe el producto');
-    }
-}
-/*
-Función “buscarProducto(nombre)” que devuelve 
-el objeto con la información del producto.
-*/
-function buscarProducto(nombre) {
-    if (nombre in productos) {
-        alert(
-            'Datos del producto ' + "\n" + "\n"
-            + 'Nombre: ' + nombre + "\n"
-            + 'Cantidad: ' + productos[nombre].cantidad + "\n"
-            + 'Precio: ' + productos[nombre].precio + "\n"
-            + 'Categoria: ' + productos[nombre].categoria + "\n"
-        );
-    } else {
-        alert('No existe ese producto');
-    }
-}
-
-/* 
-Función “actualizarInventario(nombre, cantidad)” incrementa o decrementa 
-el stock según la cantidad indicada. Si la reducción deja el stock a 0 se 
-mostrará un alert solicitando la reposición. 
-*/
-function actualizarInventario(nombre, cantidad) {
-    if (nombre in productos) {
-        productos[nombre].cantidad += cantidad;
-        alert('Producto actualizado ');
-        buscarProducto(nombre);
-
-        if (productos[nombre].cantidad == 0) {
-            alert('No hay stock, se necesita reposicion');
+        for (let nombre in productos) {
+            let producto = productos[nombre];
+            if (producto.categoria === categoria) {
+                productosCategoria += "Nombre: " + nombre + "\n" +
+                    "Cantidad: " + producto.cantidad + "\n" +
+                    "Precio: " + producto.precio + "\n" + "\n";
+            }
         }
-    } else {
-        alert('No existe ese producto, no se realizara ningun cambio');
+        alert(productosCategoria);
     }
-}
 
-/*
-Función “ordenarProductosPorPrecio()” devuelve una lista ordenada de forma
-ascendente. El resultado es un nuevo array, cuidado que no tienes el nombre.
-*/
-function ordenarProductosPorPrecio() {
-    let datosEnArray = Object.entries(productos);
-    let datosFormateados = datosEnArray.map(([nombre, valor]) => ({nombre, ...valor}));
-    let datosOrdenados = datosFormateados.sort((a, b) => a.precio - b.precio);
+    return {
+        agregarProducto,
+        eliminarProducto,
+        buscarProducto,
+        actualizarInventario,
+        ordenarProductosPorPrecio,
+        imprimirInventario,
+        filtrarProductosPorCategoria
+    };
+})();
 
-    return datosOrdenados;
-}
+window.addEventListener("load", function() {
 
-/*
-Función “imprimirInventario()” devuelve una lista con el informe de inventario, 
-el cual muestra el “nombre”, “categoría”, “cantidad”, “precio” y “total” 
-(cantidad*precio).
-*/
-function imprimirInventario() {
-    for (let nombre in productos) {
-        let producto = productos[nombre];
-        alert(
-            "Datos de " + nombre + ": " + "\n" +
-            "Nombre: " + nombre + "\n" +
-            "Cantidad: " + producto.cantidad + "\n" +
-            "Precio: " + producto.precio + "\n" +
-            "Categoría: " + producto.categoria + "\n" +
-            "Total: " + (producto.cantidad * producto.precio) + "\n"
-        );
-    }
-}
+    document.getElementById("formAgregar").addEventListener("submit", function(event) {
+        event.preventDefault();
+        const nombre = document.getElementById("nombreAgregar").value;
+        const cantidad = parseInt(document.getElementById("cantidadAgregar").value);
+        const precio = parseFloat(document.getElementById("precioAgregar").value);
+        const categoria = document.getElementById("categoriaAgregar").value;
 
-//hago un imprimir inventario para el array ordenado
-function imprimirInventarioOrdenado(array2) {
-    let reporte = '';
-    Object.entries(array2).forEach(([nombre, element]) => {
-      reporte += 'Producto: ' + element.nombre +
-        '\nCategoría: ' + element.categoria +
-        '\nCantidad: ' + element.cantidad +
-        '\nPrecio: ' + element.precio +
-        '\nTotal: ' + (element.precio * element.cantidad) +
-        '\n\n';
+        $negocio.agregarProducto(nombre, cantidad, precio, categoria);
     });
-    alert(reporte); // Devuelve el informe
-  };
 
-function filtrarProductosPorCategoria(categoria) {
-    let productosCategoria = "Productos de la categoria: " + categoria + "\n" + "\n";
+    document.getElementById("formBuscar").addEventListener("submit", function(event) {
+        event.preventDefault();
+        const nombre = document.getElementById("nombreBuscar").value;
+        $negocio.buscarProducto(nombre);
+    });
 
-    for (let nombre in productos) {
-        let producto = productos[nombre];
-        if (producto.categoria === categoria) {
-            productosCategoria += "Nombre: " + nombre + "\n" +
-                "Cantidad: " + producto.cantidad + "\n" +
-                "Precio: " + producto.precio + "\n" + "\n";
-        }
-    }
-    alert(productosCategoria);
-}
+    document.getElementById("formEliminar").addEventListener("submit", function(event) {
+        event.preventDefault();
+        const nombre = document.getElementById("nombreEliminar").value;
+        $negocio.eliminarProducto(nombre);
+    });
 
+    document.getElementById("formActualizar").addEventListener("submit", function(event) {
+        event.preventDefault();
+        const nombre = document.getElementById("nombreActualizar").value;
+        const cantidad = parseInt(document.getElementById("cantidadActualizar").value);
 
+        $negocio.actualizarInventario(nombre, cantidad);
+    });
 
-agregarProducto('manzana', 10, 0.30, 'fruta');
-agregarProducto('tortilla', 1, 1.12, 'platos');
-agregarProducto('pepino', 4, 0.60, 'verdura');
-agregarProducto('sandia', 1, 1, 'fruta');
+    document.getElementById("ordenarPorPrecio").addEventListener("click", function() {
+        $negocio.ordenarProductosPorPrecio();
+    });
 
-let array = ordenarProductosPorPrecio();
-imprimirInventarioOrdenado(array); 
+    document.getElementById("imprimirInventario").addEventListener("click", function() {
+        $negocio.imprimirInventario();
+    });
+
+    document.getElementById("filtrarCategoria").addEventListener("click", function() {
+        preventDefault();
+        const categoria = document.getElementById("categoriaFiltrar").value;
+        $negocio.filtrarProductosPorCategoria(categoria);
+    });
+});
